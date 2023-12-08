e8f0d91d933ee4c144b710d942208f346875effb
Marcus André <hello+github@marcusandre.de>
2023-12-08 19:50:06 +0100

    restart

--- init.lua
+++ init.lua
@@ -1,7 +1,2 @@
 -- https://github.com/neovim/neovim/pull/22668
 if vim.fn.has('nvim-0.9') == 1 then vim.loader.enable() end
-
-require('m.globals')
-require('m.settings')
-require('m.keymaps')
-require('m.lazy')
