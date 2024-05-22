127635343235929fef347d80f4be3a8ccfa3a09d
Marcus André <hello+github@marcusandre.de>
2024-05-22 17:21:55 +0200

    fugitive: add additional keymaps

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -4,7 +4,19 @@ return {
     vim.keymap.set("n", "<leader>gg", "<Cmd>Git<CR>", { desc = "Git" })
     vim.keymap.set("n", "g<space>", ":Git<Space>", { desc = "Git Command" })
     vim.keymap.set("n", "gX", "<Cmd>Gread<CR>", { desc = "Git Reset Buffer" })
-    vim.keymap.set("n", "<leader>gl", ":Git log --oneline<CR>", { desc = "Log" })
-    vim.keymap.set("n", "<leader>gL", ":Git log --oneline --follow -- %<CR>", { desc = "Log Buffer" })
+    vim.keymap.set("n", "<leader>gl", "<Cmd>Git log --oneline<CR>", { desc = "Log" })
+    vim.keymap.set("n", "<leader>gL", "<Cmd>Git log --oneline --follow -- %<CR>", { desc = "Log Buffer" })
+    vim.keymap.set("n", "<leader>gb", "<Cmd>Git blame<CR>", { desc = "Blame" })
+
+    local map = require("m.utils").map
+    vim.api.nvim_create_autocmd("FileType", {
+
+      pattern = { "fugitiveblame", "fugitive" },
+      callback = function()
+        map("n", "q", "<Cmd>quit<CR>", {
+          buffer = 0,
+        })
+      end,
+    })
   end,
 }
