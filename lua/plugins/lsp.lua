7f730b0371ee5745eda84944af7c65a84fa286f4
Marcus André <hello+github@marcusandre.de>
2024-06-01 11:29:51 +0200

    conform: move conform settings into own file

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -9,9 +9,6 @@ return {
 
     { "j-hui/fidget.nvim", opts = {} },
 
-    -- Autoformatting
-    "stevearc/conform.nvim",
-
     -- Schema information
     "b0o/SchemaStore.nvim",
   },
@@ -192,36 +189,5 @@ return {
         if disable_semantic_tokens[filetype] then client.server_capabilities.semanticTokensProvider = nil end
       end,
     })
-
-    -- Autoformatting Setup
-    require("conform").setup({
-      formatters_by_ft = {
-        lua = { "stylua" },
-        json = { "prettier" },
-        typescript = { "eslint" },
-      },
-      format_on_save = {
-        lsp_fallback = true,
-      },
-    })
-
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      pattern = "*",
-      callback = function(args) require("conform").format({ bufnr = args.buf }) end,
-    })
-
-    vim.api.nvim_create_user_command("Format", function(args)
-      local range = nil
-      if args.count ~= -1 then
-        local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
-        range = {
-          start = { args.line1, 0 },
-          ["end"] = { args.line2, end_line:len() },
-        }
-      end
-      require("conform").format({ async = true, lsp_fallback = true, range = range })
-    end, { range = true })
-
-    vim.keymap.set("n", "<leader>lf", "<Cmd>Format<CR>", { desc = "Format" })
   end,
 }
